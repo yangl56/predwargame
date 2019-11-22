4b8448624d7046c2d9a261c6320231564ab8d36a
yangl56 <yangl56@mcmaster.ca>
2019-11-21 23:18:08 -0500

    set up of pred war game

---
+++ predwargame.java
@@ -0,0 +1,21 @@
+package predwargame;
+
+public class predwargame {
+
+	public static void main(String[] args) {
+		my_player(0, 0, 0, 0, 0);
+	}
+	
+	private static String my_player(int x, int y, int z, int r, float b) {
+		if(r < 10000) {
+			//x, y, and z must be between 1 and 10
+			
+			//receive x tokens if my player betrays and other player betrays
+			
+			//receive y tokens if my player cooperates and other player cooperates
+			
+			//receive z tokens if my player betrays and other player cooperates
+		}
+		return "test";
+	}
+}
